b881e6b7e2a5a34ef89454ab80b33fe01bf9a481
Aksingh098 <reheat8725@gmail.com>
2026-07-15 17:42:36 +0530

    structure and design of detail and forcast card

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const CONFIG = {
     API_KEY: "077199c5c577b1d609650c043e5d79db",
     BASE_URL: "https://api.openweathermap.org/data/2.5",
     ICON_URL: "https://openweathermap.org/img/wn",
-    UNITS: "metric", // metric = Celsius
+    UNITS: "metric", 
 };
+
 
