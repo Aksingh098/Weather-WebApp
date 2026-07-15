c728d705f142cbc7d522bf7b6a214dc3300f581b
Aksingh098 <reheat8725@gmail.com>
2026-07-15 14:41:39 +0530

    hero card

---
+++ script.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    API_KEY: "077199c5c577b1d609650c043e5d79db",
+    BASE_URL: "https://api.openweathermap.org/data/2.5",
+    ICON_URL: "https://openweathermap.org/img/wn",
+    UNITS: "metric", // metric = Celsius
+};
+
